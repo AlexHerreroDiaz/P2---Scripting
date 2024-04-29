23ca28f1157974cf1e9d3a2a770c025d11d0801a
AlexHerreroDiaz <92034915+AlexHerreroDiaz@users.noreply.github.com>
2024-04-29 19:56:58 +0200

    Commit Game

---
+++ Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HayMachine : MonoBehaviour
+{
+    
+    
+    public float movementSpeed;
+    public float horizontalBoundary = 22;
+
+    public GameObject hayBalePrefab; // 1
+    public GameObject specialHayBalePrefab;
+    public Transform haySpawnpoint; // 2
+    public float shootInterval; // 3
+    private float shootTimer; // 4
+
+    public Transform modelParent; // 1
+
+    // 2
+    public GameObject blueModelPrefab;
+    public GameObject yellowModelPrefab;
+    public GameObject redModelPrefab;
+
+    public Text specialShootText; // Reference to the UI Text element
+
+    public bool isSpecialShoot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadModel();
+    }
+
+    private void LoadModel()
+    {
+        Destroy(modelParent.GetChild(0).gameObject); // 1
+
+        switch (GameSettings.hayMachineColor) // 2
+        {
+            case HayMachineColor.Blue:
+                Instantiate(blueModelPrefab, modelParent);
+            break;
+
+            case HayMachineColor.Yellow:
+                Instantiate(yellowModelPrefab, modelParent);
+            break;
+
+            case HayMachineColor.Red:
+                Instantiate(redModelPrefab, modelParent);
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement ();
+        UpdateShooting();
+
+        if (specialShootText != null)
+        {
+            specialShootText.text = isSpecialShoot ? "Active" : "Not Active";
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
+
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) // 1
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) // 2
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void ShootHay()
+    {
+
+        if (isSpecialShoot) // Check if the next shoot is special
+        {
+            // Instantiate special hay bale covering the entire width of the field
+            Instantiate(specialHayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+            isSpecialShoot = false; // Reset to normal shoot
+        }
+        else
+        {
+            Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+        }
+        SoundManager.Instance.PlayShootClip();
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay(); // Normal shoot
+        }
+    }
+
+}
---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public float runSpeed; // 1
+    public float gotHayDestroyDelay; // 2
+    private bool hitByHay; // 3
+    public float dropDestroyDelay; // 1
+    private Collider myCollider; // 2
+    private Rigidbody myRigidbody; // 3
+
+    private SheepSpawner sheepSpawner;
+
+    public float heartOffset; // 1
+    public GameObject heartPrefab; // 2
+
+    public GameObject hayMachine;
+
+    public Renderer myRenderer;
+    public bool isSpecialSheep = false; 
+    public Material goldenMaterial; // Reference to the golden material
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+
+        isSpecialSheep = Random.Range(0, 100) < 10; // we have a 20% chance of being a special sheep
+        hayMachine = GameObject.Find("Hay Machine");
+
+        if (isSpecialSheep)
+        {
+            runSpeed = 15;
+            if (myRenderer != null && goldenMaterial != null)
+            {
+                myRenderer.material = goldenMaterial;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+
+    private void HitByHay()
+    {
+        sheepSpawner.RemoveSheepFromList(gameObject);
+
+        hitByHay = true; // 1
+        runSpeed = 0; // 2
+
+        Destroy(gameObject, gotHayDestroyDelay); // 3
+
+        if (isSpecialSheep && hayMachine != null)
+        {
+            // Access the HayMachine script and set isSpecialShoot to true
+            hayMachine.GetComponent<HayMachine>().isSpecialShoot = true;
+        }
+
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+        
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>();; // 1
+        tweenScale.targetScale = 0; // 2
+        tweenScale.timeToReachTarget = gotHayDestroyDelay; // 3
+        
+        SoundManager.Instance.PlaySheepHitClip();
+
+        GameStateManager.Instance.SavedSheep();
+    }
+
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag("Special Hay") && !hitByHay) // 2
+        {
+            //Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        if (other.CompareTag("Hay") && !hitByHay) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+    }
+    private void Drop()
+    {
+        GameStateManager.Instance.DroppedSheep();
+
+        sheepSpawner.RemoveSheepFromList(gameObject);
+
+        myRigidbody.isKinematic = false; // 1
+        myCollider.isTrigger = false; // 2
+        Destroy(gameObject, dropDestroyDelay); // 3
+
+        SoundManager.Instance.PlaySheepDroppedClip();
+    }
+
+    public void SetSpawner(SheepSpawner spawner)
+    {
+        sheepSpawner = spawner;
+    }
+}
